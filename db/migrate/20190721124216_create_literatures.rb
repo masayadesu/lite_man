bd39d7aff030e4ac1e876d6bf679038eb298ebea
masaya chiba <masanogit@gmail.com>
2019-07-22 14:34:44 +0900

    マイグレーション

---
+++ db/migrate/20190721124216_create_literatures.rb
@@ -0,0 +1,18 @@
+class CreateLiteratures < ActiveRecord::Migration[5.2]
+  def change
+    create_table :literatures do |t|
+      t.string :author
+      t.string :title
+      t.string :volume
+      t.integer :page
+      t.text :url
+      t.date :published
+      t.string :publish
+      t.integer :price
+      t.string :keyword
+      t.string :remarks
+
+      t.timestamps
+    end
+  end
+end
