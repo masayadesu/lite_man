05e3804455791ca00aa5ee6dd02ac583e2dad4f4
masaya chiba <masanogit@gmail.com>
2019-08-12 12:58:08 +0900

    top about画面 追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,11 @@
 Rails.application.routes.draw do
-  # get 'mypage', to: 'users#me'
-  # get 'mypage', to: 'users#show'
+
   get 'login', to: 'sessions#login'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
 
 
   get 'about', to: 'home#about'
-  # root 'home#index'
   root 'home#top'
 
   resource :account, only: %i[show edit update destroy]
@@ -15,24 +13,24 @@ Rails.application.routes.draw do
   #
 
   # resources :users do
-  #   get "search", on: :collection
+  #   get 'search', on: :collection
   # end
 
   # resources :users
   resources :users, only: %i[ new create]
   resources :literatures do
-    # get "search", on: :collection
-    # get "change_number"
+    # get 'search', on: :collection
+    # get 'change_number'
     collection do
-      get "search"
-      get "per_page"
+      get 'search'
+      get 'per_page'
       # get 'csv_output'
     end
   end
   namespace :admin do
-    # root to: "top#index"
+    # root to: 'top#index'
     resources :users do
-      get "search", on: :collection
+      get 'search', on: :collection
     end
   end
 end
