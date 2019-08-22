f3829060962faf24e51f7b34c6893a49b5c1743e
masayan <masanogit@gmail.com>
2019-08-22 21:55:15 +0900

    adminuserがnewuser作成後 そのユーザにスィッチしないよう修正

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -30,13 +30,10 @@ class Admin::UsersController < Admin::Base
   def create
     user = User.new(user_params)
     if user.save
-      session[:user_id] = user.id
       flash[:notice] = "ユーザー登録が完了しました"
-      # redirect_to admin_root_path
       redirect_to admin_users_path
     else
       flash[:error_messages] = user.errors.full_messages
-      # binding.pry
       redirect_back fallback_location: user
 
     end
@@ -45,16 +42,11 @@ class Admin::UsersController < Admin::Base
   def update
     @user = User.find_by(id: params[:id])
     @user.assign_attributes(user_params)
-    # @user.update_attributes(user_params)
-    # binding.pry
     if @user.save
-      # redirect_to admin_root_path, notice: "ユーザー情報を編集しました"
       redirect_to admin_users_path, notice: "#{@user.name}さんのユーザー情報を編集しました"
-      # binding.pry
     else
       flash[:error_messages] = user.errors.full_messages
       render "edit"
-      # binding.pry
     end
   end
   def destroy
